16fb8ed4f9299bbc8ab7698f8f6861d7cf343e84
pratas <6882743+pratas@users.noreply.github.com>
2022-04-20 11:49:12 +0100

    painters

---
+++ src/vi.c
@@ -0,0 +1,85 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include <ctype.h>
+#include <limits.h>
+#include <ctype.h>
+#include <assert.h>
+
+#include "defs.h"
+#include "vi.h"
+#include "dna.h"
+#include "mem.h"
+#include "msg.h"
+#include "buffer.h"
+#include "common.h"
+#include "strings.h"
+
+//////////////////////////////////////////////////////////////////////////////
+// - - - - - - - - - - - - - - - - - P A I N T - - - - - - - - - - - - - - - -
+void Visual(VI_PARAMETERS *P)
+  {
+  FILE *Plot = Fopen(P->output, "w");
+  uint64_t init, end;
+  Painter *Paint;
+  uint32_t tar;
+
+  P->chrSize = (uint64_t *) Calloc(P->tar->nFiles, sizeof(uint64_t));
+
+  // SET MAXIMUM FROM FILE:
+  FILE *XReader = Fopen(P->tar->names[0], "r");
+  uint64_t x_size = 0;
+  if(fscanf(XReader, "#Length %"PRIu64"", &x_size) != 1)
+    {
+    fprintf(stderr, "Error: unknown segmented file!\n");
+    exit(1);
+    }
+  P->max = x_size;
+  fclose(XReader);
+
+  SetScale(P->max);
+  Paint = CreatePainter(GetPoint(P->max), P->backColor, P->width, P->space, 
+	  P->enlarge);
+
+  PrintHead(Plot, (2 * DEFAULT_CX) + (((Paint->width + Paint->space) *
+  P->tar->nFiles) - Paint->space), Paint->size + EXTRA);
+  Rect(Plot, (2 * DEFAULT_CX) + (((Paint->width + Paint->space) *
+  P->tar->nFiles) - Paint->space), Paint->size + EXTRA, 0, 0, P->backColor);
+
+  for(tar = 0 ; tar < P->tar->nFiles ; ++tar)
+    {
+    FILE *Reader = Fopen(P->tar->names[tar], "r");
+
+    if(fscanf(Reader, "#Length %"PRIu64"\n", &x_size) != 1)
+      {
+      fprintf(stderr, "Error: unknown segmented file!\n");
+      exit(1);
+      }
+
+    P->chrSize[tar] = x_size;
+
+    while(fscanf(Reader, "%"PRIu64"\t%"PRIu64"", &init, &end) == 2)
+      {
+      Rect(Plot, Paint->width, GetPoint(end-init+1+Paint->enlarge), Paint->cx,
+      Paint->cy + GetPoint(init), GetRgbColor(LEVEL_HUE));
+      }
+
+    Chromosome(Plot, Paint->width, GetPoint(P->chrSize[tar]), Paint->cx,
+    Paint->cy, P->corner);
+
+    if(P->tar->nFiles > 0) 
+      Paint->cx += Paint->width + Paint->space;
+
+    fclose(Reader);
+    }
+
+  PrintFinal(Plot);
+
+  return;
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
---
+++ src/vi.h
@@ -0,0 +1,18 @@
+#ifndef VI_H_INCLUDED
+#define VI_H_INCLUDED
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "defs.h"
+#include "param.h"
+#include "paint.h"
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+void    Visual  (VI_PARAMETERS *);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#endif
+

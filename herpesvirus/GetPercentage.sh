090547ebb84b9e3596d0206b3ef21f4778086ac2
pratas <6882743+pratas@users.noreply.github.com>
2022-05-19 09:49:09 +0100

    percentage redudancy tables

---
+++ herpesvirus/GetPercentage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+input="$1";
+#
+IDX=0;
+TOTAL=0;
+PART=0;
+SUM_PART=0;
+while IFS= read -r line
+  do
+  if [[ $IDX == 0 ]];
+    then
+    TOTAL=`echo $line | awk '{ print $2}'`;
+    else
+    #
+    PART_1=`echo "$line" | awk '{ print $1;}'`;
+    PART_2=`echo "$line" | awk '{ print $2;}'`;
+    #
+    PART=`echo "$PART_2-$PART_1+1" | bc -l`;
+    #
+    ((SUM_PART=SUM_PART+PART));
+    #
+    fi
+  ((++IDX));
+  #
+done < "$input"
+#
+PROPORTION=`echo "scale=3;$SUM_PART / $TOTAL * 100; " | bc -l`;
+printf "Redudant parts: %s perc. [ %s (of %s )]" "$PROPORTION" "$SUM_PART" "$TOTAL";
+#
---
+++ herpesvirus/GetPercentageAll.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+declare -a GENOMES=("HSV-1" "HSV-2" "VZV" "EBV" "HCMV" "HHV6A" "HHV6B" "HHV7" "KSHV");
+#
+rm -f V-per-out.txt;
+for GENOME in "${GENOMES[@]}"
+  do
+  ./AlcoR redundancy -v --hide --color 100 --threshold $THRESHOLD --ignore 50 --dna -w 50 -m 13:50:0:1:10:0.9/3:10:0.9 $GENOME.fa > $GENOME-d.txt
+  ./AlcoR redundancy -v --hide --threshold $THRESHOLD --color 1 --ignore 50 --dna -w 50 -m 13:50:5000:1:10:0.9/3:10:0.9 $GENOME.fa > $GENOME-l.txt 
+  printf "$GENOME\t" >> V-per-out.txt ;
+  ./GetPercentage.sh $GENOME-l.txt >> V-per-out.txt ; 
+  printf "\t" >> V-per-out.txt ;
+  ./GetPercentage.sh $GENOME-d.txt >> V-per-out.txt ; 
+  printf "\n" >> V-per-out.txt ;
+  done; 
+#
+cat V-per-out.txt | awk '{ print $1"\t"$4"\t"$7"\t"$13"\t"$16"\t"$18;}'
+#

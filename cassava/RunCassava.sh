ac3816a4529249f53f5f48e714b5275fc414f936
pratas <6882743+pratas@users.noreply.github.com>
2022-05-20 15:51:03 +0100

    map2

--- cassava/RunCassava.sh
+++ cassava/RunCassava.sh
@@ -16,5 +16,5 @@ for((x=1;x<=18;++x));
   cat h2-c-d-$x.txt h2-c-l-$x.txt > h2-$x.txt;
   done
 #
-./AlcoR visual -o map1.svg -s 6 -w 18 -e 0 h1-1.txt:h2-1.txt:h1-2.txt:h2-2.txt:h1-3.txt:h2-3.txt:h1-4.txt:h2-4.txt:h1-5.txt:h2-5.txt:h1-6.txt:h2-6.txt:h1-7.txt:h2-7.txt:h1-8.txt:h2-8.txt:h1-9.txt:h2-9.txt:h1-10.txt:h2-10.txt:h1-11.txt:h2-11.txt:h1-12.txt:h2-12.txt:h1-13.txt:h2-13.txt:h1-14.txt:h2-14.txt:h1-15.txt:h2-15.txt:h1-16.txt:h2-16.txt:h1-17.txt:h2-17.txt:h1-18.txt:h2-18.txt
+./AlcoR visual -o map2.svg -s 6 -w 18 -e 0 h1-1.txt:h2-1.txt:h1-2.txt:h2-2.txt:h1-3.txt:h2-3.txt:h1-4.txt:h2-4.txt:h1-5.txt:h2-5.txt:h1-6.txt:h2-6.txt:h1-7.txt:h2-7.txt:h1-8.txt:h2-8.txt:h1-9.txt:h2-9.txt:h1-10.txt:h2-10.txt:h1-11.txt:h2-11.txt:h1-12.txt:h2-12.txt:h1-13.txt:h2-13.txt:h1-14.txt:h2-14.txt:h1-15.txt:h2-15.txt:h1-16.txt:h2-16.txt:h1-17.txt:h2-17.txt:h1-18.txt:h2-18.txt
 #
